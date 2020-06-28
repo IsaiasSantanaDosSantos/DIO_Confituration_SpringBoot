dc248826b0735911199ddeb34eab55d3f8235c08
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2020-06-28 19:52:54 -0300

    Primeiro commit do projeto Configuration Spring Boot

---
+++ src/main/java/com/digitalinnovationone3/stringbootconfig3/Stringbootconfig3Application.java
@@ -0,0 +1,13 @@
+package com.digitalinnovationone3.stringbootconfig3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stringbootconfig3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Stringbootconfig3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitalinnovationone3/stringbootconfig3/Stringbootconfig3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovationone3.stringbootconfig3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Stringbootconfig3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
